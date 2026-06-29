d66425ec63865e62ccfe336773f093c8a6006107
NOMBO Gontran <gontrannombo166@gmail.com>
2026-06-29 18:19:13 +0000

    Initialisation du Monorepo : Spring Boot, React, Android Studio

---
+++ backend/src/test/java/com/gestionSalon/GestionSalonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionSalon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionSalonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
